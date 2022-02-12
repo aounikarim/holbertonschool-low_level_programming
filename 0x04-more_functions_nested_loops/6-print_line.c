6d4f22d9d255bee479cf9704c623fb165ec3adef
aounikarim <ak.aouni@gmail.com>
2022-02-12 20:03:43 +0100

    nestedloops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - check the code
+ * @n: parametre
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	for (; n > 0; n--)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
